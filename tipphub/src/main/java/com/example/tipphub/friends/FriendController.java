6f9f6513ab139df39f46007c3e2cb7ad198ba94e
snjohaxh <jon.haxha@stud.uni-due.de>
2022-12-01 12:02:42 +0100

    switched concept

--- tipphub/src/main/java/com/example/tipphub/friends/FriendController.java
+++ tipphub/src/main/java/com/example/tipphub/friends/FriendController.java
@@ -1,41 +1,115 @@
 package com.example.tipphub.friends;
 
-import com.example.tipphub.security.services.UserDetailsImpl;
-import com.example.tipphub.security.services.UserDetailsServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FriendController {
+    private final FriendService friendService;
+    private final ModelMapper modelMapper;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    FriendService friendService;
-
-    @Autowired
-    UserDetailsServiceImpl securityService;
-
-    @PostMapping("addFriend")
-    public ResponseEntity<?> addUser(@RequestParam("friendId")String friendId, String email) throws NullPointerException{
-        UserDetailsServiceImpl currentUser = securityService.loadUserByEmail(email);
-        friendService.saveFriend(currentUser,Integer.parseInt(friendId));
-        return ResponseEntity.ok("Friend added successfully");
+    public FriendController(FriendService friendService, ModelMapper modelMapper, ObjectMapper objectMapper) {
+        this.friendService = friendService;
+        this.modelMapper = modelMapper;
+        this.objectMapper = objectMapper;
     }
 
-    @PutMapping("deleteFriend")
-    public ResponseEntity<?> deleteUser(@RequestParam("friendId")String friendId) throws NullPointerException{
-        UserDto currentUser = securityService.loadUserByUsername();
-        friendService.saveFriend(currentUser,Integer.parseInt(friendId));
-        return ResponseEntity.ok("Friend added successfully");
+    @GetMapping(value = "/friends/{id}")
+    public List<FriendsAllViewModel> findAllFriends(@PathVariable String id) throws Exception {
+        List<RelationshipServiceModel> allFriends = this.friendService.findAllUserRelationshipsWithStatus(id);
+
+        List<FriendsAllViewModel> friendsAllViewModels = allFriends.stream().map(relationshipServiceModel -> {
+            if (!relationshipServiceModel.getUserOne().getId().equals(id)) {
+                return this.modelMapper.map(relationshipServiceModel.getUserOne(), FriendsAllViewModel.class);
+            }
+
+            return this.modelMapper.map(relationshipServiceModel.getUserTwo(), FriendsAllViewModel.class);
+        }).collect(Collectors.toList());
+
+        return friendsAllViewModels;
     }
 
+    @PostMapping(value = "/addFriend")
+    public ResponseEntity addFriend(@RequestBody Map<String, Object> body) throws Exception {
+        String loggedInUserId = (String) body.get("loggedInUserId");
+        String friendCandidateId = (String) body.get("friendCandidateId");
 
-    @GetMapping("listFriends")
-    public ResponseEntity<List<User>> getFriends() {
-        List<User> myFriends = friendService.getFriends();
-        return new ResponseEntity<List<User>>(myFriends, HttpStatus.OK);
+        boolean result = this.friendService.createRequestForAddingFriend(loggedInUserId, friendCandidateId);
+
+        if (result) {
+            SuccessResponse successResponse = new SuccessResponse(LocalDateTime.now(), SUCCESSFUL_FRIEND_REQUEST_SUBMISSION_MESSAGE, "", true);
+
+            return new ResponseEntity<>(this.objectMapper.writeValueAsString(successResponse), HttpStatus.OK);
+        }
+
+        throw new CustomException(SERVER_ERROR_MESSAGE);
     }
 
+    @PostMapping(value = "/removeFriend")
+    public ResponseEntity removeFriend(@RequestBody Map<String, Object> body) throws Exception {
+        String loggedInUserId = (String) body.get("loggedInUserId");
+        String friendToRemoveId = (String) body.get("friendToRemoveId");
+
+        boolean result = this.friendService.removeFriend(loggedInUserId, friendToRemoveId);
+
+        if (result) {
+            SuccessResponse successResponse = new SuccessResponse(LocalDateTime.now(), SUCCESSFUL_FRIEND_REMOVE_MESSAGE, "", true);
+            return new ResponseEntity<>(this.objectMapper.writeValueAsString(successResponse), HttpStatus.OK);
+        }
+
+        throw new CustomException(SERVER_ERROR_MESSAGE);
+    }
+
+    @PostMapping(value = "/acceptFriend")
+    public ResponseEntity acceptFriend(@RequestBody Map<String, Object> body) throws Exception {
+        String loggedInUserId = (String) body.get("loggedInUserId");
+        String friendToAcceptId = (String) body.get("friendToAcceptId");
+
+        boolean result = this.friendService.acceptFriend(loggedInUserId, friendToAcceptId);
+
+        if (result) {
+            SuccessResponse successResponse = new SuccessResponse(LocalDateTime.now(), SUCCESSFUL_ADDED_FRIEND_MESSAGE, "", true);
+            return new ResponseEntity<>(this.objectMapper.writeValueAsString(successResponse), HttpStatus.OK);
+        }
+        throw new CustomException(SERVER_ERROR_MESSAGE);
+    }
+
+    @PostMapping(value = "/cancelRequest")
+    public ResponseEntity cancelFriendshipRequest(@RequestBody Map<String, Object> body) throws Exception {
+        String loggedInUserId = (String) body.get("loggedInUserId");
+        String friendToRejectId = (String) body.get("friendToRejectId");
+
+        boolean result = this.friendService.cancelFriendshipRequest(loggedInUserId, friendToRejectId);
+
+        if (result) {
+            SuccessResponse successResponse = new SuccessResponse(LocalDateTime.now(), SUCCESSFUL_REJECT_FRIEND_REQUEST_MESSAGE, "", true);
+            return new ResponseEntity<>(this.objectMapper.writeValueAsString(successResponse), HttpStatus.OK);
+        }
+
+        throw new CustomException(SERVER_ERROR_MESSAGE);
+    }
+
+    @PostMapping(value = "/search", produces = "application/json")
+    public List<FriendsCandidatesViewModel> searchUsers(@RequestBody Map<String, Object> body) {
+        String loggedInUserId = (String) body.get("loggedInUserId");
+        String search = (String) body.get("search");
+
+        return this.friendService.searchUsers(loggedInUserId, search);
+    }
+
+    @GetMapping(value = "/findFriends/{id}", produces = "application/json")
+    public List<FriendsCandidatesViewModel> findAllNotFriends(@PathVariable String id) {
+        return this.friendService.findAllFriendCandidates(id);
+    }
 }
