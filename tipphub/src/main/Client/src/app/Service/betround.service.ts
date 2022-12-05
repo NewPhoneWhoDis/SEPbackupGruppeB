76ee16cb32459afa809fc0451a28e4fa33b5a4b2
snjohaxh <jon.haxha@stud.uni-due.de>
2022-12-05 18:42:23 +0100

    switch

---
+++ tipphub/src/main/Client/src/app/Service/betround.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { User } from "../Model/User";
+import { RegistrationRequest } from "../Model/RegistrationRequest";
+
+const httpHeaders = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  }),
+};
+
+@Injectable({
+  providedIn: "root",
+})
+export class BetroundService {
+  betroundURL: string;
+  visible: boolean = true;
+
+  constructor(private http: HttpClient) {
+    this.betroundURL = "http://localhost:8080/betround";
+  }
+
+  public addNewBetround(
+    leagueId: number,
+    ownerId: number,
+    betround: Betround
+  ): Observable<RegistrationRequest> {
+    return this.http.put<Bet>(
+      `${this.betroundURL}/betround/${leagueId}/${ownerId}`,
+      betround
+    );
+  }
+}
---
+++ tipphub/src/main/Client/src/app/components/betround-table/betround-table.component.ts
@@ -0,0 +1,26 @@
+import { Observable } from "rxjs";
+import { LeagueService } from "../../Service/league.service";
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "betround-table",
+  templateUrl: "./betround-table.component.html",
+  styleUrls: ["./betround-table.component.css"],
+})
+export class BetroundTableComponent implements OnInit {
+  appObservable = new Observable((observer) => {
+    console.log("test");
+    observer.next("1");
+    observer.next("2");
+    observer.next("3");
+    observer.next("4");
+  });
+
+  constructor(public leagueService: LeagueService) {}
+
+  ngOnInit(): void {
+    this.appObservable.subscribe((val) => {
+      console.log(val);
+    });
+  }
+}
