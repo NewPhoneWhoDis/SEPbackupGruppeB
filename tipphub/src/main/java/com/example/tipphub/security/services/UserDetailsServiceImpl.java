3e0ab170ad648802a0e268bfe494d96f1297f0a7
Yakup Balci <yakup.balci@stud.uni-due.de>
2022-12-12 22:36:07 +0100

    Login Bug fixed

--- tipphub/src/main/java/com/example/tipphub/security/services/UserDetailsServiceImpl.java
+++ tipphub/src/main/java/com/example/tipphub/security/services/UserDetailsServiceImpl.java
@@ -24,6 +24,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
