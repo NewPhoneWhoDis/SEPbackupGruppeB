232c293c96ca7eaf476152919076bb4e18d3c484
Andrii Hrabar <andrii.hrabar@stud.uni-due.de>
2022-12-14 22:08:38 +0100

    systemDatum wird beim Starten des Programs gespeichert

--- tipphub/src/main/java/com/example/tipphub/TipphubApplication.java
+++ tipphub/src/main/java/com/example/tipphub/TipphubApplication.java
@@ -1,10 +1,17 @@
 package com.example.tipphub;
 
 
+import com.example.tipphub.hubSystem.HubSystem;
+import com.example.tipphub.hubSystem.HubSystemRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
 public class TipphubApplication {
@@ -12,13 +19,15 @@ public class TipphubApplication {
 		SpringApplication.run(TipphubApplication.class, args);
 	}
 
-
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+	@Bean
+	CommandLineRunner commandLineRunner(HubSystemRepository repository){
+		return args -> {
+			if(repository.findAll().size() < 1){
+				HubSystem system = new HubSystem(LocalDate.now());
+				repository.save(system);
+			}
+		};
 	}
-
-
-
 
 }
 
