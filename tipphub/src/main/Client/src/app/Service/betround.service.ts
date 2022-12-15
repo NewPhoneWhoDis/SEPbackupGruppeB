9cd554e0206d51ab404ce723d86dc52406e7e530
snjohaxh <jon.haxha@stud.uni-due.de>
2022-12-15 10:57:24 +0100

    added betround-creation.ts and modal

--- tipphub/src/main/Client/src/app/Service/betround.service.ts
+++ tipphub/src/main/Client/src/app/Service/betround.service.ts
@@ -1,10 +1,10 @@
-import { Betround } from './../Model/Betround';
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {League} from "../Model/League";
-import { User } from '../Model/User';
-import { Bet } from '../Model/Bet';
+import { Betround } from "./../Model/Betround";
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { League } from "../Model/League";
+import { User } from "../Model/User";
+import { Bet } from "../Model/Bet";
 
 const httpHeaders = {
   headers: new HttpHeaders({
@@ -13,34 +13,58 @@ const httpHeaders = {
   }),
 };
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class BetroundService {
-
   private betroundUrl: string;
 
   constructor(private http: HttpClient) {
-    this.betroundUrl = 'http://localhost:8080/betround';
+    this.betroundUrl = "http://localhost:8080/betround";
   }
 
   public getAllBetrounds(): Observable<Betround[]> {
     return this.http.get<Betround[]>(`${this.betroundUrl}/all`, httpHeaders);
   }
 
-  public addNewBetround(leagueId: number, ownerId: number, betround: Betround): Observable<Betround> {
-    return this.http.put<Betround>(`${this.betroundUrl}/add/${leagueId}/${ownerId}`, betround, httpHeaders);
+  public addNewBetround(
+    league: League,
+    owner: User,
+    betround: Betround
+  ): Observable<Betround> {
+    return this.http.put<Betround>(
+      `${this.betroundUrl}/add/${league.id}/${owner.id}`,
+      betround,
+      httpHeaders
+    );
   }
 
-  public betInRound(ownerId: number, betroundId: number, betround: Betround): Observable<Betround> {
-    return this.http.put<Betround>(`${this.betroundUrl}/bet/${ownerId}/${betroundId}`, betround);
+  public betInRound(
+    ownerId: number,
+    betroundId: number,
+    betround: Betround
+  ): Observable<Betround> {
+    return this.http.put<Betround>(
+      `${this.betroundUrl}/bet/${ownerId}/${betroundId}`,
+      betround
+    );
   }
 
   public getEvaluationInRound(ownerId: number, betroundId: number) {
-    return this.http.get<Betround>(`${this.betroundUrl}/evaluation/${ownerId}/${betroundId}`);
+    return this.http.get<Betround>(
+      `${this.betroundUrl}/evaluation/${ownerId}/${betroundId}`
+    );
   }
 
-  public generateInvite(betroundId: number, ownerId: number, targetetUserId: number, betround: Betround): Observable<Betround> {
-    return this.http.put<Betround>(`${this.betroundUrl}/inviteGeneration/${betroundId}/${ownerId}/${targetetUserId}`, betround);
+  public generateInvite(
+    betroundId: number,
+    ownerId: number,
+    targetetUserId: number,
+    betround: Betround
+  ): Observable<Betround> {
+    return this.http.put<Betround>(
+      `${this.betroundUrl}/inviteGeneration/${betroundId}/${ownerId}/${targetetUserId}`,
+      betround
+    );
   }
 
   //
@@ -48,12 +72,21 @@ export class BetroundService {
     return this.http.get<Betround>(`${this.betroundUrl}/getInivteURL`);
   }
 
-  public saveUserInBetrounds(userId: User, betroundId: Betround, betround: Betround): Observable<Betround> {
-    return this.http.put<Betround>(`${this.betroundUrl}/getInivteURL/${userId}/${betroundId}`, betround);
+  public saveUserInBetrounds(
+    userId: User,
+    betroundId: Betround,
+    betround: Betround
+  ): Observable<Betround> {
+    return this.http.put<Betround>(
+      `${this.betroundUrl}/getInivteURL/${userId}/${betroundId}`,
+      betround
+    );
   }
 
   public getAllParticipants(userId: number): Observable<User[]> {
-    return this.http.get<User[]>(`${this.betroundUrl}/getAllParticipants${userId}`);
+    return this.http.get<User[]>(
+      `${this.betroundUrl}/getAllParticipants${userId}`
+    );
   }
 
   /*
