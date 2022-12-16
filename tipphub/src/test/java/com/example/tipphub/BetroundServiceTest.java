ce9759bfab8fcbec631793dda0915348e7862822
Ali <ali.oezbey@stud.uni-due.de>
2022-12-16 16:30:03 +0100

    Unit Test for addNewBetround

--- tipphub/src/test/java/com/example/tipphub/BetroundServiceTest.java
+++ tipphub/src/test/java/com/example/tipphub/BetroundServiceTest.java
@@ -1,18 +1,26 @@
 package com.example.tipphub;
 
-import com.example.tipphub.betround.Betround;
-import com.example.tipphub.betround.BetroundController;
-import com.example.tipphub.betround.BetroundRepository;
-import com.example.tipphub.betround.BetroundService;
+import com.example.tipphub.betround.*;
+import com.example.tipphub.email.EmailSenderService;
+import com.example.tipphub.hubSystem.HubSystemRepository;
+import com.example.tipphub.league.GameRepository;
 import com.example.tipphub.league.League;
+import com.example.tipphub.league.LeagueRepository;
 import com.example.tipphub.user.User;
-import org.junit.jupiter.api.Assertions;
+import com.example.tipphub.user.UserRepository;
+import com.example.tipphub.user.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = TipphubApplication.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -37,14 +45,38 @@ public class BetroundServiceTest {
 
     @Test
     @DisplayName("Test Should Pass When New Betround is added")
-    void shouldAddNewBetround() {
+    public void testAddNewBetround() {
+        // Create mock objects
+        LeagueRepository mockLeagueRepository = Mockito.mock(LeagueRepository.class);
+        UserRepository mockUserRepository = Mockito.mock(UserRepository.class);
+        BetroundRepository mockBetroundRepository = Mockito.mock(BetroundRepository.class);
+        BetRepository mockBetRepository = Mockito.mock(BetRepository.class);
+        GameRepository mockGameRepository = Mockito.mock(GameRepository.class);
+        HubSystemRepository mockHubSystemRepo = Mockito.mock(HubSystemRepository.class);
+        EmailSenderService mockSender = Mockito.mock(EmailSenderService.class);
+        UserService mockUser = Mockito.mock(UserService.class);
 
-        Betround betround= Mockito.mock(Betround.class);
-        League league= Mockito.mock(League.class);
-        User owner= Mockito.mock(User.class);
 
-        betroundService.addNewBetround(league.getId(),owner.getId(),betround);
-        Assertions.assertNotNull(betroundRepository.findById(betround.getId()).get());
+        // Set up mock behavior
+        when(mockLeagueRepository.findById(anyLong())).thenReturn(Optional.of(Mockito.mock(League.class)));
+        when(mockUserRepository.findById(anyLong())).thenReturn(Optional.of(Mockito.mock(User.class)));
+        when(mockBetroundRepository.save(any(Betround.class))).thenReturn(Mockito.mock(Betround.class));
+        when(mockBetRepository.save(any(Bet.class))).thenReturn(Mockito.mock(Bet.class));
+
+        // Create instance of the class under test
+        BetroundService service = new BetroundService(mockBetroundRepository, mockBetRepository,
+                mockUserRepository, mockLeagueRepository, mockHubSystemRepo, mockGameRepository,
+                mockSender, mockUser
+        );
+
+        // Call the method under test
+        service.addNewBetround(1L, 2L, Mockito.mock(Betround.class));
+
+        // Verify that the mock objects were called as expected
+        verify(mockLeagueRepository, times(1)).findById(1L);
+        verify(mockUserRepository, times(1)).findById(2L);
+        verify(mockBetroundRepository, times(1)).save(any(Betround.class));
+
 
     }
 
