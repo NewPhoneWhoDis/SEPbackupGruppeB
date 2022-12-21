672b2609ea1140a0f517112a39fa082885d71f7c
Kristiyan <88944722+NewPhoneWhoDis@users.noreply.github.com>
2022-12-21 15:48:14 +0100

    Added service and component logic for chat feature

--- tipphub/src/main/Client/src/app/Service/message.service.ts
+++ tipphub/src/main/Client/src/app/Service/message.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { User } from '../Model/User';
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +22,9 @@ export class MessageService {
 
   public getSpecificUserAllMessages(): Observable<string[]> {
     return this.http.get<string[]>(`${this.messageUrl}/all`)
+  }
+
+  public saveMessageInDatabase(message: string, user: User) {
+    this.http.put(`${this.messageUrl}/save`, user);
   }
 }
--- tipphub/src/main/Client/src/app/components/chat-modal/chat-modal.component.ts
+++ tipphub/src/main/Client/src/app/components/chat-modal/chat-modal.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from 'src/app/Model/User';
+import { MessageService } from 'src/app/Service/message.service';
+import { StorageService } from 'src/app/Service/storage.service';
+import { UserService } from 'src/app/Service/user.service';
 
 @Component({
   selector: 'app-chat-modal',
@@ -12,23 +15,32 @@ export class ChatModalComponent implements OnInit {
   currentUser : User | undefined;
   friendUser: User | undefined;
 
-  constructor() {
+  constructor(private messageService: MessageService, 
+    private userService: UserService,
+    private storageService: StorageService) {
   }
 
   ngOnInit(): void {
-    
+    this.userService.getUserById(this.storageService.getLoggedUser()).subscribe(data =>{this.currentUser = data});
   }
 
-  sendMessage(message: string) {
-    //* Inject message service
+  sendMessage(message: string, currentUserId: number | undefined) {
+    this.currentUserMessages.push(message);
+    this.saveMessage(message, currentUserId as number);
   }
 
   getMessages() {
-    /* Inject message service
-    this.http.get<Array<String>>('/test/messages').subscribe(messages => {
-      this.messages = messages;
-    });
+    /*
+    this.messageService.getAllCurrentUserMessages().subscribe(data => {
+      this.currentUserMessages = data;
+    })
     */
+  }
+
+  saveMessage(message: string, currentUserId: number) {
+    let authorOfMessage: User = new User();
+    this.userService.getUserById(currentUserId as number).subscribe(data => {authorOfMessage = data});
+    this.messageService.saveMessageInDatabase(message, authorOfMessage);
   }
 
 }
