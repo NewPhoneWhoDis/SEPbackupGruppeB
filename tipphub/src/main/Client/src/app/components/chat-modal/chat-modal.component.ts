26f1d1c861840dd587afb811641a33a7fe1b5af9
Kristiyan <88944722+NewPhoneWhoDis@users.noreply.github.com>
2022-12-21 13:03:01 +0100

    created chat component

---
+++ tipphub/src/main/Client/src/app/components/chat-modal/chat-modal.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-modal',
+  templateUrl: './chat-modal.component.html',
+  styleUrls: ['./chat-modal.component.css']
+})
+export class ChatModalComponent implements OnInit {
+
+  constructor() {
+   }
+
+  ngOnInit(): void {
+    
+  }
+
+}
