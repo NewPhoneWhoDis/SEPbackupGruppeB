8fd851c53877aa9d3435358a7c3a2c624f8c2c48
Yakup Balci <yakup.balci@stud.uni-due.de>
2022-12-26 16:42:58 +0100

    Add intervals work

--- tipphub/src/main/Client/src/app/components/minigame/minigame.component.ts
+++ tipphub/src/main/Client/src/app/components/minigame/minigame.component.ts
@@ -19,34 +19,62 @@ export class MinigameComponent implements OnInit {
 
   }
 
-  onHit(){
-    this.points += 10;
-    this.resetBall();
-    console.log("ball hit")
-  }
-
   startGame(){
     this.gameStarted = true;
-    this.isVisible = true;
-    interval(1000).subscribe(() => {
+    this.moveBall();
+    let int = interval(1000).subscribe(() => {
       if(this.secondsPassed >= 1){
         this.secondsPassed --;
+      }
+      if(this.secondsPassed === 0){
+        this.endGame();
+        int.unsubscribe();
       }
     })
   }
 
-  spawnBall(){
+  moveBall(){
+    this.isVisible = true;
 
+    let obs = interval(1000).subscribe((data) => {
+      setInterval(()=>{
+      let ball = document.getElementById('ball');
+      if(ball){
+        ball.style.top = ball.offsetTop + 1 + "px";
+        ball.style.left = ball.offsetLeft + 1 + "px";
+      }
+    },100)
+
+      if(!this.isVisible){
+        obs.unsubscribe();
+      }
+      if(data === 4){
+        this.resetBall();
+        obs.unsubscribe();
+      }
+    })
+  }
+
+  onHit(){
+    this.points += 10;
+    this.resetBall();
   }
 
   resetBall(){
     this.isVisible = false;
     let obs = interval(1000).subscribe((data) => {
       if(data === 9){
-        console.log(data)
-        this.isVisible = true;
-        obs.unsubscribe()
+        this.moveBall();
+        obs.unsubscribe();
+      }
+      if(!this.gameStarted){
+        obs.unsubscribe();
       }
     })
+  }
+
+  endGame(){
+    this.gameStarted = false;
+    this.isVisible = false;
   }
 }
