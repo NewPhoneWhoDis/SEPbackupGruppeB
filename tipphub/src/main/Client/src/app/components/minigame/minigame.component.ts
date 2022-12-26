b9bd92165cc64a9a2306e54fc51b841ff6d56dd2
Yakup Balci <yakup.balci@stud.uni-due.de>
2022-12-26 15:46:47 +0100

    Add timer and onHit

--- tipphub/src/main/Client/src/app/components/minigame/minigame.component.ts
+++ tipphub/src/main/Client/src/app/components/minigame/minigame.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {interval} from "rxjs";
 
 @Component({
   selector: 'app-minigame',
@@ -7,12 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MinigameComponent implements OnInit {
 
+  isVisible : boolean = false;
+  secondsPassed : number = 120;
+  gameStarted : boolean = false;
+  points : number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
-  test(){
-    console.log("test successfull")
+  onHit(){
+    this.points += 10;
+    this.resetBall();
+    console.log("ball hit")
+  }
+
+  startGame(){
+    this.gameStarted = true;
+    this.isVisible = true;
+    interval(1000).subscribe(() => {
+      if(this.secondsPassed >= 1){
+        this.secondsPassed --;
+      }
+    })
+  }
+
+  spawnBall(){
+
+  }
+
+  resetBall(){
+    this.isVisible = false;
+    let obs = interval(1000).subscribe((data) => {
+      if(data === 9){
+        console.log(data)
+        this.isVisible = true;
+        obs.unsubscribe()
+      }
+    })
   }
 }
