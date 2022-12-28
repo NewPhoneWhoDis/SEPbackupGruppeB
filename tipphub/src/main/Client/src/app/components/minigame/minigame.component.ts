1e0b5b6222c1d129cd147ad8b3709777b9cf6339
Yakup Balci <yakup.balci@stud.uni-due.de>
2022-12-28 14:46:32 +0100

    Add bouncing physics

--- tipphub/src/main/Client/src/app/components/minigame/minigame.component.ts
+++ tipphub/src/main/Client/src/app/components/minigame/minigame.component.ts
@@ -9,10 +9,11 @@ import {interval} from "rxjs";
 export class MinigameComponent implements OnInit {
 
   isVisible : boolean = false;
-  secondsPassed : number = 120;
+  secondsPassed : number = 60;
   gameStarted : boolean = false;
   points : number = 0;
-
+  x_vel : number = 5;
+  y_vel : number = 5;
   constructor() { }
 
   ngOnInit(): void {
@@ -22,7 +23,7 @@ export class MinigameComponent implements OnInit {
   startGame(){
     this.gameStarted = true;
     this.moveBall();
-    let int = interval(1000).subscribe(() => {
+    let int = interval(1000).subscribe((data) => {
       if(this.secondsPassed >= 1){
         this.secondsPassed --;
       }
@@ -33,26 +34,55 @@ export class MinigameComponent implements OnInit {
     })
   }
 
-  moveBall(){
-    this.isVisible = true;
-
-    let obs = interval(1000).subscribe((data) => {
-      setInterval(()=>{
-      let ball = document.getElementById('ball');
-      if(ball){
-        ball.style.top = ball.offsetTop + 1 + "px";
-        ball.style.left = ball.offsetLeft + 1 + "px";
-      }
-    },100)
-
-      if(!this.isVisible){
-        obs.unsubscribe();
-      }
+  startBallTimer(){
+    let int = interval(1000).subscribe((data) => {
       if(data === 4){
         this.resetBall();
-        obs.unsubscribe();
       }
-    })
+      if(!this.isVisible){
+        int.unsubscribe();
+      }
+    });
+  }
+
+  moveBall(){
+    this.isVisible = true;
+    this.startBallTimer();
+
+    let obs = setInterval(()=>{
+      let ball = document.getElementById('ball');
+      let field = document.getElementById('field');
+
+      if(ball && field){
+        // on Collision do this
+        let ball_height = ball.offsetHeight;
+        let ball_width = ball.offsetWidth;
+        let ball_top = ball.offsetTop;
+        let ball_left = ball.offsetLeft;
+        let field_height = field.offsetHeight;
+        let field_width = field.offsetWidth;
+        let field_top = field.offsetTop;
+        let field_left = field.offsetLeft;
+
+        if(ball_left <= field_left || ball_left >= field_width){
+          this.x_vel = this.x_vel *(-1)
+        }
+
+        if (ball_top <= field_top || ball_top >= field_height){
+          this.y_vel = this.y_vel *(-1);
+        }
+
+        //move ball diagonally
+        ball.style.top = ball.offsetTop + this.y_vel + "px";
+        ball.style.left = ball.offsetLeft + this.x_vel + "px";
+        console.log(ball.style.top)
+        console.log(ball.style.left)
+      }
+      if(!this.isVisible){
+        clearInterval(obs);
+      }
+    },25)
+
   }
 
   onHit(){
@@ -76,5 +106,8 @@ export class MinigameComponent implements OnInit {
   endGame(){
     this.gameStarted = false;
     this.isVisible = false;
+    this.secondsPassed = 60;
+    // todo add points to balance
+    this.points = 0;
   }
 }
