cf6cb636ce9b7c59848e026e7d59ec30b0ec2bea
Kristiyan <88944722+NewPhoneWhoDis@users.noreply.github.com>
2023-01-14 00:31:48 +0100

    Basic building blocks of the chat component

--- tipphub/src/main/Client/src/app/components/chat/group-chat/group-chat.component.ts
+++ tipphub/src/main/Client/src/app/components/chat/group-chat/group-chat.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { StorageService } from './../../../Service/storage.service';
+import { User } from 'src/app/Model/User';
+import { UserService } from './../../../Service/user.service';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-group-chat',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupChatComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('messageInput')
+  messageInput!: { nativeElement: { value: string; }; };
+  //@Input() currUserId: number | undefined = 1;
+  //@Input() friendOfCurrUserId: number | undefined = 1;
+  currentUser: User | undefined;
+
+  constructor(private userService: UserService, private storageService: StorageService) { }
 
   ngOnInit(): void {
+    this.userService.getUserById(this.storageService.getLoggedUser()).subscribe(data =>{this.currentUser = data});
   }
 
 }
