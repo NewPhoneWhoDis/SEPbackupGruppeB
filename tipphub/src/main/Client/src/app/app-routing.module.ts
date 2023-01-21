2622fe749f59aebb598022eab0c110974df31287
Kristiyan <88944722+NewPhoneWhoDis@users.noreply.github.com>
2023-01-21 20:52:29 +0100

    Registered new route and bug fixes

--- tipphub/src/main/Client/src/app/app-routing.module.ts
+++ tipphub/src/main/Client/src/app/app-routing.module.ts
@@ -4,14 +4,12 @@ import { RouterModule, Routes } from "@angular/router";
 import { betroundPage } from "./components/pages/betroundPage/betroundPage";
 import { homePage } from "./components/pages/homePage/homePage";
 import {GameTableBetsComponent} from "./components/game-table-bets/game-table-bets.component";
-import { GroupChatComponent } from './components/chat/group-chat/group-chat.component';
 
 const routes: Routes = [
   { path: "", component: homePage },
   { path: "bets-and-pieces", component: betroundPage },
   { path: "ligen-management", component: LeageCreationPageComponent },
   { path: "test", component: GameTableBetsComponent },
-  //{ path: "group-chat", component: GroupChatComponent}
 ];
 
 @NgModule({
