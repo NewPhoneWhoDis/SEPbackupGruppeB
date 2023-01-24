c8cb271523dfc5c31fb29c01a369edc7fe084685
snjohaxh <jon.haxha@stud.uni-due.de>
2023-01-24 22:36:52 +0100

    added getBetAmountPerUser method to service

--- tipphub/src/main/Client/src/app/Service/betround.service.ts
+++ tipphub/src/main/Client/src/app/Service/betround.service.ts
@@ -1,12 +1,11 @@
-import { Betround } from './../Model/Betround';
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {League} from "../Model/League";
-import { User } from '../Model/User';
-import { Bet } from '../Model/Bet';
-import {Game} from "../Model/Game";
-
+import { Betround } from "./../Model/Betround";
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { League } from "../Model/League";
+import { User } from "../Model/User";
+import { Bet } from "../Model/Bet";
+import { Game } from "../Model/Game";
 
 const httpHeaders = {
   headers: new HttpHeaders({
@@ -28,60 +27,137 @@ export class BetroundService {
     return this.http.get<Betround[]>(`${this.betroundUrl}/all`, httpHeaders);
   }
 
-  public addNewBetround(league: League,owner: User, betround: Betround): Observable<Betround> {
-    return this.http.put<Betround>(`${this.betroundUrl}/add/${league.id}/${owner.id}`,betround,httpHeaders);
+  public addNewBetround(
+    league: League,
+    owner: User,
+    betround: Betround
+  ): Observable<Betround> {
+    return this.http.put<Betround>(
+      `${this.betroundUrl}/add/${league.id}/${owner.id}`,
+      betround,
+      httpHeaders
+    );
   }
 
-  public betInRound(ownerId: number, betroundId: number, bet: Bet): Observable<any> {
-    return this.http.put<Betround>(`${this.betroundUrl}/bet/${ownerId}/${betroundId}`, bet, httpHeaders);
+  public betInRound(
+    ownerId: number,
+    betroundId: number,
+    bet: Bet
+  ): Observable<any> {
+    return this.http.put<Betround>(
+      `${this.betroundUrl}/bet/${ownerId}/${betroundId}`,
+      bet,
+      httpHeaders
+    );
   }
 
-  public getEvaluationInRound(ownerId: number, betroundId: number): Observable<number> {
-    return this.http.get<number>(`${this.betroundUrl}/evaluation/${ownerId}/${betroundId}`, httpHeaders);
+  public getEvaluationInRound(
+    ownerId: number,
+    betroundId: number
+  ): Observable<number> {
+    return this.http.get<number>(
+      `${this.betroundUrl}/evaluation/${ownerId}/${betroundId}`,
+      httpHeaders
+    );
   }
 
-  public getBestBetters(leaugeId: number | undefined): Observable<Array<string>>{
-    return this.http.get<Array<string>>(`${this.betroundUrl}/getBest/${leaugeId}`, httpHeaders);
+  public getBestBetters(
+    leaugeId: number | undefined
+  ): Observable<Array<string>> {
+    return this.http.get<Array<string>>(
+      `${this.betroundUrl}/getBest/${leaugeId}`,
+      httpHeaders
+    );
   }
 
-  public getBestTeams(leagueId: number | undefined): Observable<Array<string>>{
-    return this.http.get<Array<string>>(`${this.betroundUrl}/getTopThreeTeams/${leagueId}`, httpHeaders);
+  public getBestTeams(leagueId: number | undefined): Observable<Array<string>> {
+    return this.http.get<Array<string>>(
+      `${this.betroundUrl}/getTopThreeTeams/${leagueId}`,
+      httpHeaders
+    );
   }
 
-
-  public getAllParticipants(betroundId: number | undefined): Observable<User[]> {
-    return this.http.get<User[]>(`${this.betroundUrl}/getAllParticipants/${betroundId}`, httpHeaders);
+  public getAllParticipants(
+    betroundId: number | undefined
+  ): Observable<User[]> {
+    return this.http.get<User[]>(
+      `${this.betroundUrl}/getAllParticipants/${betroundId}`,
+      httpHeaders
+    );
   }
 
-
-  public getBetHelp(gameId: number): Observable<Game>{
-    return this.http.get<Game>(`${this.betroundUrl}/getBetHelp/${gameId}`, httpHeaders);
+  public getBetHelp(gameId: number): Observable<Game> {
+    return this.http.get<Game>(
+      `${this.betroundUrl}/getBetHelp/${gameId}`,
+      httpHeaders
+    );
   }
 
   public sendEmailInviteBetround(betroundId: number, targetUserId: number) {
     ///onLinkClick/{userId}/{betroundId}
-    return this.http.get(`${this.betroundUrl}/getInivteURL/${betroundId}/${targetUserId}`);
-  } 
-
-  public setNickname(userId: number, betroundId: number, nickname: string): Observable<any> {
-    return this.http.put<any>(`${this.betroundUrl}/setNickname/${userId}/${betroundId}?nickname=${nickname}`, httpHeaders);
+    return this.http.get(
+      `${this.betroundUrl}/getInivteURL/${betroundId}/${targetUserId}`
+    );
   }
 
-  public shareBet(friendId: number | undefined, betId: number | undefined): Observable<any>{
-    let shareBetUrl = this.betroundUrl.replace("betround","notification/shareBet");
-    return this.http.put<any>(`${shareBetUrl}/${friendId}/${betId}`,null,httpHeaders);
+  public setNickname(
+    userId: number,
+    betroundId: number,
+    nickname: string
+  ): Observable<any> {
+    return this.http.put<any>(
+      `${this.betroundUrl}/setNickname/${userId}/${betroundId}?nickname=${nickname}`,
+      httpHeaders
+    );
+  }
+
+  public shareBet(
+    friendId: number | undefined,
+    betId: number | undefined
+  ): Observable<any> {
+    let shareBetUrl = this.betroundUrl.replace(
+      "betround",
+      "notification/shareBet"
+    );
+    return this.http.put<any>(
+      `${shareBetUrl}/${friendId}/${betId}`,
+      null,
+      httpHeaders
+    );
   }
 
   public getNickname(userId: number, betroundId: number): Observable<any> {
-    var headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
-    return this.http.get<any>(`${this.betroundUrl}/getNickname/${userId}/${betroundId}`,  {headers, responseType: 'text' as 'json'});
-  }
-  
-  public getLeagueId(betroundId: number) {
-    return this.http.get<number>(`${this.betroundUrl}/getLeagueId/${betroundId}`, httpHeaders);
+    var headers = new HttpHeaders().set(
+      "Content-Type",
+      "text/plain; charset=utf-8"
+    );
+    return this.http.get<any>(
+      `${this.betroundUrl}/getNickname/${userId}/${betroundId}`,
+      { headers, responseType: "text" as "json" }
+    );
   }
 
-  public addUserToBetround(betroundId: number | undefined, userId: number){
-    return this.http.get<string>(`${this.betroundUrl}/onLinkClick/${betroundId}/${userId}`,httpHeaders);
+  public getLeagueId(betroundId: number) {
+    return this.http.get<number>(
+      `${this.betroundUrl}/getLeagueId/${betroundId}`,
+      httpHeaders
+    );
+  }
+
+  public addUserToBetround(betroundId: number | undefined, userId: number) {
+    return this.http.get<string>(
+      `${this.betroundUrl}/onLinkClick/${betroundId}/${userId}`,
+      httpHeaders
+    );
+  }
+
+  public getBetAmountPerUserInRound(
+    userId: number,
+    betroundId: number | undefined
+  ) {
+    return this.http.get<string>(
+      `${this.betroundUrl}/getAmountOfBetsPerUserInRound/${userId}/${betroundId}`,
+      httpHeaders
+    );
   }
 }
